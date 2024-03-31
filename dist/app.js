4e11df00f59a178b4beeca50245ae52dde8cb15e
brymmobaggins <ibro2tablet21@yahoo.com>
2024-03-31 13:47:34 +0100

    cart functionality

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,90 @@
 const btn = document.querySelector('button.mobile-menu-button');
 const menu = document.querySelector('.mobile-menu');
+const addtoCartBtn = document.querySelectorAll("#add-to-cart");
 
 btn.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
-/**
- * showSlides() displays a slideshow by looping through an array of slide elements
- * and displaying one slide at a time. It sets the display to 'none' for all slides,
- * increments the slideIndex, wraps around when reaching the end, displays the current
- * slide, and calls itself again after 2 seconds using setTimeout().
- */
+
+
+for (let i = 0; i < addtoCartBtn.length; i++) {
+  addtoCartBtn[i].addEventListener('click', () => {
+    cardNumber()
+  });
+}
+
+function cardNumber() {
+  let productNumbers = localStorage.getItem('cartNumbers');
+
+  productNumbers = parseInt(productNumbers);
+
+  if (productNumbers) {
+    localStorage.setItem('cartNumbers', productNumbers + 1);
+    document.getElementById("items-count").textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem('cartNumbers', 1);
+    document.getElementById("items-count").textContent = 1;
+  }
+
+}
+const products =
+  [{
+    id: 1,
+    name: "Belington",
+    price: 40000,
+    description: "Belington"
+  },
+  {
+    id: 2,
+    name: "Alaskan Malamute",
+    price: 750000,
+    description: "Alaskan Malamute"
+  },
+  {
+    id: 3,
+    name: "American Eskimo",
+    price: 405000,
+    description: "American Eskimo"
+  },
+  {
+    id: 4,
+    name: "Border Collie",
+    price: 305000,
+    description: "Border Collie"
+  },
+  {
+    id: 5,
+    name: "Jack Rusell Terrier",
+    price: 505000,
+    description: "Alaskan Malamute"
+  },
+  {
+    id: 6,
+    name: "Shiba Inu",
+    price: 805000,
+    description: "Shiba Inu"
+  },
+  {
+    id: 7,
+    name: "Rottweiler",
+    price: 650000,
+    description: "Rottweiler"
+  },
+  {
+    id: 8,
+    name: "Dalmatian",
+    price: 465000,
+    description: "Dalmatian"
+  }
+  ]
+
+
+
+
+
+
+
+
 let slideIndex = 0;
 // showSlides();
 
