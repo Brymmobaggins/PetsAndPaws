89d60d84d3331ffea263f623ee6f61e525c17352
brymmobaggins <ibro2tablet21@yahoo.com>
2024-04-08 14:39:15 +0100

    update image

--- dist/app.js
+++ dist/app.js
@@ -230,7 +230,7 @@ function displayCart() {
            <td class=""><span>&#x20A6;</span>${item.inCart * item.price}</td>
            <td class="text-center">
            
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-6 text-red-400 hover:text-red-300 hover:cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="#ff9494" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-6 text-red-400 hover:text-red-400 hover:cursor-pointer">
                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
               </svg>
 
@@ -258,14 +258,12 @@ function displayCart() {
           <h5>Your cart is currently empty</h5>
        </div>
        <div class="flex h-[200px] item-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-25 h-[200px]">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-         </svg>
-       </div>
+         <img src="/images/empty-cart-img.jpeg" alt="">       
+         </div>
        <div class="text-center text-xs font-light">
           <span>You may check out some available product and buy in the shop</span>
        </div>
-      <a href="index.html" class="decoration border-2 rounded-full bg-alabaster-400 hover:bg-alabaster-300 text-center text-sm block w-1/4 mb-2 m-auto py-1">Return to shop</a>
+      <a href="index.html" class="border-2 rounded-full bg-amethyst-600 hover:bg-alabaster-300 text-center text-white text-sm block w-1/4 mb-2 m-auto py-1">Return to shop</a>
 
     </div>
     `
